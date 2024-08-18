2610b307b20d47952592309a55ace3df4111ec5a
eachekalina <eachekalina@edu.hse.ru>
2024-08-18 11:55:53 +0300

    fix tests

--- frontend/tests/src/test/java/FrontendTests.java
+++ frontend/tests/src/test/java/FrontendTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +13,9 @@ public class FrontendTests {
 
     @BeforeEach
     public void setup() {
-        driver = new ChromeDriver();
+        var options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
